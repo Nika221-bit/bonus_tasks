e30eb14bfc17d32f4a833d3e505b76c6455e0c0b
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-17 00:20:40 +0400

    1

---
+++ main.js
@@ -0,0 +1,13 @@
+//task 1
+
+let arr = [1, 2, 3, 2, 4, 1, 5, 3];
+let unique = arr.filter((num, index) => arr.indexOf(num) === index);
+console.log(unique);
+
+//task 2 
+
+let ArrOfStrings = ["html","JavaScript","css"]
+
+const Longest = ArrOfStrings.filter(filter(Word){
+     
+})
